2377e95bb7e376db0c56879d568050c6ece2a3ce
Dylan Nirvana home <dylannirvanahome@Dylans-MacBook-Pro.local>
2020-05-07 11:52:23 -0400

    removes scss from local

--- client/src/index.js
+++ client/src/index.js
@@ -10,8 +10,8 @@ import {
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
-// import './index.css';
-import './custom.scss'; // Works! Compiled by WebPack????
+import './index.css';
+// import './custom.scss'; // Works! Compiled by WebPack????
 import App from './App';
 import Story from './routes/story';
 import Media from './routes/media';

558ae1a323f9b38406c29bb00720fa5091754059
Dylan Nirvana <dylannirvana@Marigold.local>
2020-05-13 17:10:26 -0400

    adds html audio placeholder

--- client/src/routes/media.js
+++ client/src/routes/media.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import News from '../components/News'
+// import Audio from '../components/Audio'
 
 // NOTE: media.js takes the place of both artist and client rosters (which are galleries) until they get enough (artists and clients) to show. (Until then, artists.js and clients.js should not be accessible, but can be ready to go!) 
 // NOTE: media.js aggregates VDO, audio, twitter, publicity. This provides the WHY should I do the CTA (which is what contact them? Do online form?) This is API not UX

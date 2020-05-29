615029d66d400ae1117c5f11cd49a079c6433611
Dylan Nirvana <dylannirvana@Marigold.local>
2020-05-29 09:31:37 -0400

    blocker on api come back later

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -12,7 +12,7 @@ class News extends Component {
   }
 
   componentDidMount() {
-    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=b1d3b456717c4a2e921dd4cb0379e828';
+    const url = 'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=b1d3b456717c4a2e921dd4cb0379e828';
     // Use REST Client plugin (or Postman) to identify Singleton if needed
     fetch(url)
       .then((response) => {

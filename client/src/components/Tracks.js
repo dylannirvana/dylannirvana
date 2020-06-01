92c352a6e7d218e6727fb361e365d44c8a8750b3
Dylan Nirvana <dylannirvana@Marigold.local>
2020-06-01 15:30:29 -0400

    adds artist scene placeholders

--- client/src/components/Tracks.js
+++ client/src/components/Tracks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import wave from '../wave.gif';
 import { 
 } from 'react-router-dom';
 
@@ -7,9 +8,26 @@ class Tracks extends React.Component {
         return (
             <div>
                 <h2>Tracks </h2>
-               
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut nulla eaque cum laudantium enim! Labore aliquid numquam rem perferendis, harum excepturi veniam eos eligendi blanditiis corrupti soluta reprehenderit qui voluptate.</p>
                 
+                {/* BOOTSTRAP */}
+                <main role="main" className="container">
+                    <div style={{padding: "2rem"}} className="jumbotron">
+                        <h4>Super duper track 1</h4>
+                        <p className="lead">This example is a quick exercise to illustrate how fixed to top navbar works.</p>
+                        <div>
+                            <img src={wave} className="container" alt="track" />
+                        </div>
+                    </div>
+
+                    <div style={{padding: "2rem"}} className="jumbotron">
+                        <h4>Super duper track 2</h4>
+                        <p className="lead">This example is a quick exercise to illustrate how fixed to top navbar works.</p>
+                        <div>
+                            <img src={wave} className="container" alt="track" />
+                        </div>
+                    </div>
+                </main>
             </div>
         )
     }

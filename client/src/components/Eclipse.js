e150836464e3a2bf1560dd5a8817a4d144515545
Dylan Nirvana <dylannirvana@MacBook-Pro.local>
2020-09-03 21:48:17 -0400

    adds eclipse show

---
+++ client/src/components/Eclipse.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+
+class Eclipse extends React.Component {
+    render() {
+        return (
+            <div >
+                <Container className="section">
+                    <h2>Dylan Nirvana: Live From Eclipse. </h2>
+
+                    <p>Dylan Nirvana is the last and final performer in the series Live From Eclipse, airing tonight Friday Sept 4. Two shows: 7pm and 9pm .</p>
+
+                    <p>Live From Eclipse has raised over $10,000 for Saint Augustine musicians, many of whom lost their livelihood due to the pandemic. The series will continue to broadcast and make money for the over 33 contributing musicians, thanks to your generous donations.</p>
+
+                    <p>Special thanks to Jim Stafford, Angie and Uller Bailey for keeping the flame of music alive. Please tell your friends about Live From Eclipse and please come to this live online event!! </p>
+
+                    <h5>Here is the <a href="https://www.facebook.com/events/690086791580620">Facebook Event.</a> Or just go to the live broadcast Friday Sept 4 at 7pm and 9pm at <a href="https://eclipserecording.com/live/">Eclipse Studios</a></h5>
+                    
+                    
+                </Container>
+               </div>
+
+               
+        )
+    }
+}
+
+export default Eclipse;

9ef33fc629f7832c33a6a67cc8cc094d039a19ed
Dylan Nirvana <dylannirvana@Nirvana.local>
2021-03-11 14:26:43 -0500

    cta soundcloud

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -23,7 +23,7 @@ class Footer extends React.Component {
                     </div> */}
 
                      <Nav className="justify-content-center footer" >
-                        {/* <Nav.Item><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/dylannirvana/"><i className="fab fa-instagram fa-2x"></i></a></Nav.Item> */}
+                        <Nav.Item><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/dylannirvana/"><i className="fab fa-instagram fa-2x"></i></a></Nav.Item>
                         {/* <Nav.Item><a target="_blank" rel="noopener noreferrer" href="https://soundcloud.com/dylannirvana"><i className="fab fa-soundcloud fa-2x"></i></a></Nav.Item> */}
                         {/* <Nav.Item><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/TheBadFlowers/"><i className="fab fa-facebook fa-2x"></i></a></Nav.Item> */}
                         {/* <Nav.Item><a target="_blank" rel="noopener noreferrer" href="https://twitter.com/DylanNirvana"><i className="fab fa-twitter fa-2x"></i></a></Nav.Item> */}

04c0f66b7216b2cadf3dfe16038b3d147211e4bf
Dylan Nirvana <dylannirvana@Nirvana.local>
2021-04-24 23:31:12 -0400

    adds facebook shopify verification in head

---
+++ client/src/components/Bandsintown.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Bandsintown extends Component {
+
+    render() {
+        return (
+            <div>
+                {/* BandsInTown widget */}
+                <script charset="utf-8" src="https://widget.bandsintown.com/main.min.js"></script><a class="bit-widget-initializer" data-artist-name="Dylan Nirvana and the Bad Flowers" data-display-local-dates="false" data-display-past-dates="false" data-auto-style="false" data-text-color="#000000" data-link-color="#00b4b3" data-background-color="rgba(0,0,0,0)" data-display-limit="15" data-display-start-time="false" data-link-text-color="#FFFFFF" data-display-lineup="false" data-display-play-my-city="true" data-separator-color="rgba(124,124,124,0.25)"></a>
+            </div>
+        )
+    }
+}
+
+export default Bandsintown
